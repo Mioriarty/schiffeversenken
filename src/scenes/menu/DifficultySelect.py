530e62cc0157b910b022843a3ce8f5f637f1a298
Mioriarty <moritz.seppelt@googlemail.com>
2022-01-30 14:38:16 +0100

    + add description to menu scene

--- src/scenes/menu/DifficultySelect.py
+++ src/scenes/menu/DifficultySelect.py
@@ -12,7 +12,8 @@ from utils.Transform import Transform
 
 class DifficultySelect(Component):
 
-    TEXT_SCALE = (0.5, 0.5)
+    NAME_SCALE = (0.5, 0.5)
+    DESCRIPTION_SCALE = (0.4, 0.4)
     MOVE_DURATION = 0.2
 
     def __init__(self, transform : Transform = None):
@@ -20,10 +21,13 @@ class DifficultySelect(Component):
 
         self.selectedIndex = 0
 
-        self.nameImages = [ Sprite(img, Transform(scale=DifficultySelect.TEXT_SCALE, parent=self.transform), bakeNow=True) for img in Difficulties.allNameImages() ]
+        self.nameImages = [ Sprite(img, Transform(scale=DifficultySelect.NAME_SCALE, parent=self.transform), bakeNow=True) for img in Difficulties.allNameImages() ]
+        self.descitpionImages = [ Sprite(img, Transform((0, 100), scale=DifficultySelect.DESCRIPTION_SCALE, parent=self.transform), bakeNow=True) for img in Difficulties.allDescriptionIamges() ]
         for i in range(1, len(self.nameImages)):
             self.nameImages[i].image.set_alpha(0)
+            self.descitpionImages[i].image.set_alpha(0)
         SceneManager.putInDrawLayer(self.nameImages, SceneManager.UI_MAIN_LAYER)
+        SceneManager.putInDrawLayer(self.descitpionImages, SceneManager.UI_MAIN_LAYER)
         
         self.appearLeftMove = Animator.easeOut(np.array((-250, 0)), np.zeros(2 ), DifficultySelect.MOVE_DURATION)
         self.appearRightMove = Animator.easeOut(np.array((250, 0)), np.zeros(2), DifficultySelect.MOVE_DURATION)
@@ -32,6 +36,9 @@ class DifficultySelect(Component):
         self.disappearLeftMove = Animator.easeIn(np.zeros(2), np.array((-250, 0)), DifficultySelect.MOVE_DURATION)
         self.disappearRightMove = Animator.easeIn(np.zeros(2), np.array((250, 0)), DifficultySelect.MOVE_DURATION)
         self.disappearAlpha = Animator.lerp(255., 0., DifficultySelect.MOVE_DURATION)
+
+        self.desAppear   = Animator.const(0, DifficultySelect.MOVE_DURATION / 2) + Animator.lerp(0., 255., DifficultySelect.MOVE_DURATION / 2)
+        self.desDisppear = Animator.lerp(255., 0., DifficultySelect.MOVE_DURATION / 2)
         
         self.leftButton = ImageButton(Sprite("buttons.left"), transform = Transform((-300, 0), scale=(0.2, 0.2), parent=self.transform))
         self.leftButton.setOnClickEvent(self.leftPress)
@@ -44,16 +51,20 @@ class DifficultySelect(Component):
 
         self.disappearLeftMove.setHook(self.nameImages[self.selectedIndex].transform.setRelPosition)
         self.disappearAlpha.setHook(self.nameImages[self.selectedIndex].image.set_alpha)
+        self.desDisppear.setHook(self.descitpionImages[self.selectedIndex].image.set_alpha)
 
         self.selectedIndex = (self.selectedIndex + 1) % len(self.nameImages)
 
         self.appearRightMove.setHook(self.nameImages[self.selectedIndex].transform.setRelPosition)
         self.appearAlpha.setHook(self.nameImages[self.selectedIndex].image.set_alpha)
+        self.desAppear.setHook(self.descitpionImages[self.selectedIndex].image.set_alpha)
 
         self.disappearLeftMove.replay()
         self.disappearAlpha.replay()
+        self.desDisppear.replay()
         self.appearRightMove.replay()
         self.appearAlpha.replay()
+        self.desAppear.replay()
 
         Difficulties.setSelectedIndex(self.selectedIndex)
         
@@ -63,16 +74,20 @@ class DifficultySelect(Component):
 
         self.disappearRightMove.setHook(self.nameImages[self.selectedIndex].transform.setRelPosition)
         self.disappearAlpha.setHook(self.nameImages[self.selectedIndex].image.set_alpha)
+        self.desDisppear.setHook(self.descitpionImages[self.selectedIndex].image.set_alpha)
 
         self.selectedIndex = (self.selectedIndex - 1 + len(self.nameImages)) % len(self.nameImages)
 
         self.appearLeftMove.setHook(self.nameImages[self.selectedIndex].transform.setRelPosition)
         self.appearAlpha.setHook(self.nameImages[self.selectedIndex].image.set_alpha)
+        self.desAppear.setHook(self.descitpionImages[self.selectedIndex].image.set_alpha)
 
         self.disappearRightMove.replay()
         self.disappearAlpha.replay()
+        self.desDisppear.replay()
         self.appearLeftMove.replay()
         self.appearAlpha.replay()
+        self.desAppear.replay()
 
         Difficulties.setSelectedIndex(self.selectedIndex)
     
@@ -83,6 +98,8 @@ class DifficultySelect(Component):
 
             self.disappearAlpha.skipToEnd()
             self.appearAlpha.skipToEnd()
+            self.desAppear.skipToEnd()
+            self.desDisppear.skipToEnd()
 
         elif self.disappearRightMove.isRunning():
             self.disappearRightMove.skipToEnd()
@@ -90,5 +107,6 @@ class DifficultySelect(Component):
 
             self.disappearAlpha.skipToEnd()
             self.appearAlpha.skipToEnd()
-        
+            self.desAppear.skipToEnd()
+            self.desDisppear.skipToEnd()
         
--- src/scenes/menu/MenuScene.py
+++ src/scenes/menu/MenuScene.py
@@ -24,10 +24,10 @@ class MenuScene(Scene):
         self.headingSprite = Sprite("texts.seeschlacht", Transform.screenCenter(y = 100., scale=(0.5, 0.5), parent=self.parentTransform), bakeNow=True)
         SceneManager.putInDrawLayer(self.headingSprite, SceneManager.UI_MAIN_LAYER)
 
-        self.gengnerSprite = Sprite("texts.gegner", Transform.screenCenter(y=230., scale=(0.3, 0.3), parent=self.parentTransform), bakeNow=True)
-        SceneManager.putInDrawLayer(self.gengnerSprite, SceneManager.UI_MAIN_LAYER)
+        # self.gengnerSprite = Sprite("texts.gegner", Transform.screenCenter(y=230., scale=(0.3, 0.3), parent=self.parentTransform), bakeNow=True)
+        # SceneManager.putInDrawLayer(self.gengnerSprite, SceneManager.UI_MAIN_LAYER)
     
-        self.difficultySelect = DifficultySelect(Transform.screenCenter(y = 320., parent=self.parentTransform))
+        self.difficultySelect = DifficultySelect(Transform.screenCenter(y = 280., parent=self.parentTransform))
 
         self.islands = [
             Diashow([ "ambient.island1", "ambient.island2" ], 0.5, Transform((100, 200), scale=(0.4, 0.4), parent=self.parentTransform)),
@@ -35,7 +35,7 @@ class MenuScene(Scene):
         ]
         SceneManager.putInDrawLayer(self.islands, SceneManager.UI_MAIN_LAYER)
 
-        self.playBtn = ImageButton(Sprite("buttons.spielen"), transform = Transform.screenCenter(y=500., scale=(0.3, 0.3), parent=self.parentTransform))
+        self.playBtn = ImageButton(Sprite("buttons.spielen"), transform = Transform.screenCenter(y=600., scale=(0.3, 0.3), parent=self.parentTransform))
         self.playBtn.setOnClickEvent(self.gameStartAnimation.play)
 
         self.birds = [
