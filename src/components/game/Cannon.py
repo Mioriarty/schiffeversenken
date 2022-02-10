cda96953ae0dceafad1d50296dc471163eb8853a
Mioriarty <moritz.seppelt@googlemail.com>
2022-02-10 15:58:13 +0100

    + implemented the cannon

---
+++ src/components/game/Cannon.py
@@ -0,0 +1,17 @@
+from matplotlib.animation import Animation
+from components.ambient.Diashow import Diashow
+from utils.Animator import Animator
+from utils.Transform import Transform
+
+
+class Cannon(Diashow):
+
+    OPENING_POS = (44., 1.)
+
+    def __init__(self, duration: float = 0.1, transform: Transform = None):
+        super().__init__([ "game.cannon0", "game.cannon1", "game.cannon2", "game.cannon3", "game.cannon4" ], duration, False, transform)
+        self.animation.setRepeatMode(Animator.STOP)
+
+
+    def getOpeningPos(self) -> tuple[float]:
+        return self.transform.apply(Cannon.OPENING_POS)
