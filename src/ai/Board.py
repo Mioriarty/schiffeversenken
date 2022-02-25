d832685e966fa5bc3c45aa855feab61c78e27b84
Mioriarty <moritz.seppelt@googlemail.com>
2022-02-25 18:13:27 +0100

    + comments

--- src/ai/Board.py
+++ src/ai/Board.py
@@ -3,6 +3,15 @@ from typing import Generator
 
 
 class Board:
+    """
+    Holds possible states of any cell of a board as well as stretegic states.
+
+    Attributes:
+        width (int): Amount of columns.
+        height (int): Amount of rows.
+        data (list[int]): All stored states
+
+    """
 
     # internal values
     NO_INFO         = 0b00001
@@ -16,34 +25,93 @@ class Board:
     SUBMIT_NO_SHIP = CHECKED_NO_SHIP
 
     def __init__(self, width : int, height : int):
+        """
+        Constructor of the Board class.
+
+        Args:
+            width (int): Amount of columns.
+            height (int): Amount of rows.
+        """
         self.width = width
         self.height = height
 
         self.data = [[Board.NO_INFO for _ in range(width)] for _ in range(height)]
     
     def __getitem__(self, cell : tuple[int]) -> int:
+        """
+        Gets the state of a specified cell.
+
+        Args:
+            cell (tuple[int]): x and y coordinate of the cell
+
+        Returns:
+            int: Current state of the specified cell.
+        """
         return self.data[cell[0]][cell[1]]
     
     def __setitem__(self, cell : tuple[int], newVal : int) -> None:
+        """
+        Gets the state of a specified cell.
+
+        Args:
+            cell (tuple[int]): x and y coordinate of the cell
+            newVal (int): New state of the specified cell
+        """
         self.data[cell[0]][cell[1]] = newVal
     
     def check(self, cell : tuple[int], value : int) -> bool:
+        """
+        Checks if the cell is in one or more states.
+
+        To submit multiple states, seperate them with |. Example: check((x, y), Board.NO_INFO | Board.DEDUSED_NO_SHIP)
+
+        Args:
+            cell (tuple[int]): x and y coordinate of the cell
+            value (int): On ore more states (seperated by |) that the states should e checked against.
+
+        Returns:
+            bool: If the states cell is included in the specified states .
+        """
         return self.isInBounds(cell) and bool(self[cell] & value)
     
     def isInBounds(self, cell : tuple[int]) -> bool:
+        """
+        Checks if a certain coordinate is in bounds of the board.
+
+        Args:
+            cell (tuple[int]): x and y coordinate to be checked against.
+
+        Returns:
+            bool: Ifthecoordinate is in bounds of the board.
+        """
         return cell[0] >= 0 and cell[1] >= 0 and cell[0] < self.width and cell[1] < self.height
 
     def shuffledIndex(self) -> Generator[tuple[int], None, None]:
+        """
+        Enables to loop through all cells in the board randomly.
+
+        Yields:
+            Generator[tuple[int], None, None]: All cells on the board in a random order.
+        """
         for y in random.sample(list(range(self.height)), self.height):
             for x in random.sample(list(range(self.width)), self.width):
                 yield (x, y)
     
     def orderedIndex(self) -> Generator[tuple[int], None, None]:
+        """
+        Enables to loop through all cells in the board in order.
+
+        Yields:
+            Generator[tuple[int], None, None]: All cells on the board in order.
+        """
         for y in range(self.height):
             for x in range(self.width):
                 yield (x, y)
 
-    def print(self):
+    def print(self) -> None:
+        """
+        Prints the board's state to the console.
+        """
         for y in range(self.height):
             for x in range(self.width):
                 val = self.data[x][y]
--- src/components/Component.py
+++ src/components/Component.py
@@ -3,17 +3,56 @@ import pygame
 
 
 class Component(metaclass = utils.InstanceRegistryMetaClass):
+    """
+        Represents a plain component with a certain Transform.
+
+        By deriving from it, it is possible to add behavior and graphics to it.
+        To do that, just override the update and draw methods.
+        
+        The update method will be called automatically by the Registry. For the draw method to be called, it has to by registered in the SceneManager using putInDrawLayer.
+
+        Attributes:
+            transform (Transform): The position, rotation and scale of the Component. In some curcumstances this might be not necessary. In this case just use None as the Transform. This will create an identity TRansform for the Component.
+    """
 
     def __init__(self, transform : utils.Transform):
+        """
+        Constructor of the Component class.
+
+        Args:
+            transform (Transform): The position, rotation and scale of the Component. In some curcumstances this might be not necessary. In this case just use None as the Transform. This will create an identity TRansform for the Component.
+        """
         self.transform : utils.Transform = utils.Transform.fromTransform(transform)
 
     def update(self, dt : float) -> None:
+        """
+        Holds the behavior of the Component. It should be overwritten.
+
+        It will be called automatically by the Registry
+
+        Args:
+            dt (float): Time that past since the last frame in seconds.
+        """
         pass
 
     def draw(self, screen : pygame.Surface) -> None:
+        """
+        Hold any graphical behavior of the Component. It should be overwritten.
+        
+        For it to be called, it has to by registered in the SceneManager using putInDrawLayer
+
+        Args:
+            screen (pygame.Surface): The screen surface oh which you should draw using pygame.
+        """
         pass
 
     @staticmethod
     def updateAll(dt : float) -> None:
+        """
+        Calls the update method of all Components in the Registry. Should only be called by main.
+
+        Args:
+            dt (float): Time that past since the last frame in seconds.
+        """
         for c in Component._instances:
             c.update(dt)
