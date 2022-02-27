1cb6fa09d112c69fc1111ad652c2134c133aa736
Mioriarty <moritz.seppelt@googlemail.com>
2022-02-27 19:03:53 +0100

    + comments - test code

--- src/ai/AiMaster.py
+++ src/ai/AiMaster.py
@@ -10,10 +10,31 @@ import random
 from ai.BruteForceGameAi import BruteForceGameAi
 
 class AiMaster:
+    """
+    Decides which ai to use when.
+
+    Possible Ai's:
+        Random Ai: When a mistake should be made.
+        Classic Ai: Shoots as a human would do. (checkerboard pattern, completes ships, etc)
+        BruteForce Ai: Tries all combinations of ships and shoots where a ship is in most of these combionations. As the calculations are quite heavy, it only starts when 3 Ships are left.
+        Pro Ai: Just like BruteForce, but it doesn't pay attention to whether the ships overlap and thus drastically reduce computation time. It can be used from the beginning. It won't finish ships so if this ai is used, a Classic Ai will be kept up to date so it can handle found ships. 
+        ShipPlacing Ai: Places the ships on the board. It will try to minimize the blocked area.
+    """
 
     BRUTE_FORCE_SHIP_THRESHOLD = 3
 
     def __init__(self, boardWidth : int, boardHeight : int, chanceOfMistake : float, numShipPlacementTries : int, useProAi : bool, numShips : dict[int, int] = {2: 4, 3: 3, 4: 2, 5: 1}):
+        """
+        Constructor of the AiMaster class.
+
+        Args:
+            boardWidth (int): Amount of columns of the board.
+            boardHeight (int): Amount of rows of the board.
+            chanceOfMistake (float): Likelyhood that a random shot happens when getNextShot is called.
+            numShipPlacementTries (int): How many tries should the ship Placing Ai have to minimize the blocked area.
+            useProAi (bool): Determines if the Pro Ai will be used.
+            numShips (_type_, optional): How many ships of which length are in the game. The key is the length of the ships and values is the number of that kind of ships. Defaults to {2: 4, 3: 3, 4: 2, 5: 1}.
+        """
         self.board = Board(boardWidth, boardHeight)
         self.chanceOfMistake = chanceOfMistake
         self.numShips = numShips.copy()
@@ -27,6 +48,12 @@ class AiMaster:
         self.bruteForceMode = False
 
     def getNextShot(self) -> tuple[int]:
+        """
+        Calculates the next shot. 
+
+        Returns:
+            tuple[int]: Next shot position / tile.
+        """
         if random.random() < self.chanceOfMistake:
             return self.randomAi.getNextShot(self.board)
         
@@ -40,6 +67,13 @@ class AiMaster:
                 return self.classicAi.getNextShot(self.board)
     
     def submitInfo(self, pos : tuple[int], state : int) -> None:
+        """
+        Submits and forwards a new cell information to the ais.
+
+        Args:
+            pos (tuple[int]): The x and y coordinate of the cell in question.
+            state (int): The found state. Either Board.SUBMIT_SHIP or Board.SUBMIT_NO_SHIP
+        """
         if self.bruteForceMode:
             self.bruteForceAi.submitInfo(pos, state)
         else:
@@ -48,9 +82,24 @@ class AiMaster:
         self.board[pos] = state
 
     def generateShipPlacement(self) -> ShipPlacement:
+        """
+        Return the pre-calculated ShipPlacement.
+
+        Returns:
+            ShipPlacement: ShipPlacement used in the game.
+        """
         return self.shipPlacingAi.get()
     
     def __shouldUseBruteForce(self) -> bool:
+        """
+        Returns whether the BruteForce Ai should be used for a shot.
+
+        It can also kick of  its generation.
+
+        Returns:
+            bool: If the BruteForce Ai should be used for a shot.
+        """
+
         # it will start using the brute force ai if t was already used or
         # - only a certain numbers of unknown ships left AND
         # - no SHIP_LIKELY tile is on the board
@@ -71,6 +120,9 @@ class AiMaster:
         return False
     
     def printState(self) -> None:
+        """
+        Prints the current ai state for debugging purpose.
+        """
         if self.bruteForceMode:
             print("Brute Force State")
             print(self.bruteForceAi.cellPropabilities)
@@ -81,27 +133,4 @@ class AiMaster:
             self.board.print()
             print(self.classicAi.numShips)
         print()
-
-if __name__ == "__main__":
-
-    ai = AiMaster(11, 11, 0)
-    placement = ai.generateShipPlacement()
-    placement.print(ai.board.width, ai.board.height)
-
-    bfAi = BruteForceGameAi()
-
-    while True:
-        z = input()
-        if z == "now":
-            bfAi.kickOffGeneration(ai.board, {2: 4, 3: 3, 4: 2, 5: 1})
-
-
-        shot = ai.getNextShot()
-        val = Board.SUBMIT_SHIP if placement.cellOccupied(shot) else Board.SUBMIT_NO_SHIP
-        ai.submitInfo(shot, val)
-
-        ai.board.print()
-        
-
-        
         
