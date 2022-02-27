5330b58646cae9a2ac412400d6163b2d38a13fa0
Mioriarty <moritz.seppelt@googlemail.com>
2022-02-27 19:11:28 +0100

    + comments

--- src/ai/BruteForceGameAi.py
+++ src/ai/BruteForceGameAi.py
@@ -6,10 +6,16 @@ from threading import Thread
 
 
 class BruteForceGameAi:
+    """
+    To calculate the next shot it tries all valid ship placements and selects the most likely tile.
+    """
 
     MAX_POSSIBLE_SHIP_LOCATIONS = 16
 
     def __init__(self):
+        """
+        The constructor of the BruteForceGameAi class.
+        """
         self.possiblePlacements : set[ShipPlacement] = None
         self.cellPropabilities = {}
         self.generateThread = None
@@ -18,6 +24,16 @@ class BruteForceGameAi:
 
     
     def kickOffGeneration(self, board : Board, numShipsLeft : dict[int, int]) -> bool:
+        """
+        Kicks off the generation of all possible ship placements. Abords if there are propabably to many to handle.
+
+        Args:
+            board (Board): The current board state
+            numShipsLeft (dict[int, int]):  How many ships of which length are left to place. The key is the length of the ships and values is the number of that kind of ships.
+
+        Returns:
+            bool: Whether the creation will be successfull.
+        """
         self.possiblePlacements = set()
 
         possibleShipLocations = self.__getAllPossibleShipLocations(board)
@@ -41,10 +57,16 @@ class BruteForceGameAi:
 
         return True
 
-    def generationDone(self) -> bool:
-        return self.generateThread is not None and not self.generateThread.is_alive()
-
     def __generatePossiblePlacements(self, shipsToDo : list[int], crntPlacement : ShipPlacement, possibleShipLocations : list[tuple[int]], board : Board):
+        """
+        Recursively generates all possible ship placaments.
+
+        Args:
+            shipsToDo (list[int]): Ship length's that are left to place on the board
+            crntPlacement (ShipPlacement): Placement of all the ships that allready have been placed.
+            possibleShipLocations (list[tuple[int]]): All possible tiles where a ship can be.
+            board (Board): The current state of the board.
+        """
         if len(shipsToDo) == 0:
             self.__submitPossibleShipPlacement(crntPlacement)
             return
@@ -73,9 +95,24 @@ class BruteForceGameAi:
                 self.__generatePossiblePlacements(shipsToDo.copy(), tempPlacement, possibleShipLocations, board)
 
     def __getAllPossibleShipLocations(self, board : Board) -> list[tuple[int]]:
+        """
+        Returns all possible tiles where a ship can be.
+
+        Args:
+            board (Board): The current state of the board.
+
+        Returns:
+            list[tuple[int]]: All possible tiles where a ship can be.
+        """
         return [ (x, y) for x, y in board.orderedIndex() if board.check((x, y), Board.NO_INFO) and (board.check((x+1, y), Board.NO_INFO) or board.check((x, y+1), Board.NO_INFO)) ]
 
     def __submitPossibleShipPlacement(self, placement : ShipPlacement) -> None:
+        """
+        Adds placement to the list and updates all cells total.
+
+        Args:
+            placement (ShipPlacement): Placement to be added.
+        """
         self.possiblePlacements.add(placement)
         
         for cell in placement.occupiedCells():
@@ -85,6 +122,15 @@ class BruteForceGameAi:
                 self.cellPropabilities[cell] = 1
     
     def getNextShot(self):
+        """
+        Calculates the next shot. 
+
+        Raises:
+            RuntimeError: If the generation hasnt been kicked off yet.
+
+        Returns:
+            tuple[int]: Next shot position / tile.
+        """
         if self.generateThread is None:
             raise RuntimeError("The Brute Force Ai hasnt been started yet so it cannot advice a shot position")
 
@@ -101,6 +147,13 @@ class BruteForceGameAi:
 
     
     def submitInfo(self, pos : tuple[int], state : int) -> None:
+        """
+        Submits and forwards a new cell information to the ais.
+
+        Args:
+            pos (tuple[int]): The x and y coordinate of the cell in question.
+            state (int): The found state. Either Board.SUBMIT_SHIP or Board.SUBMIT_NO_SHIP
+        """
         if self.generateThread is not None:
             self.generateThread.join()
 
@@ -115,6 +168,12 @@ class BruteForceGameAi:
             del self.cellPropabilities[pos]
 
     def __removePossibleShipPlacement(self, placement : ShipPlacement) -> None:
+        """
+        Removes placement from the list and updates all cells total.
+
+        Args:
+            placement (ShipPlacement): Placement to be removed.
+        """
         for cell in placement.occupiedCells():
             if cell in self.cellPropabilities:
                 self.cellPropabilities[cell] -= 1
